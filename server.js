8ddf2b35940aa08bae5012d7c715856482ff37e9
Waldo Mauricio BC <waldomauriciobc@gmail.com>
2024-09-22 12:40:54 -0500

    w01
    It was tested and corrected until the deployment worked.

--- server.js
+++ server.js
@@ -5,26 +5,40 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ /* Set the View Engine and templates
+ *************************/
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
+
 /* ***********************
  * Routes
  *************************/
-app.use(static)
+app.use(static);  // Use the static routes defined in static.js
+
+// Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" });
+});
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT 
+const host = process.env.HOST 
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
